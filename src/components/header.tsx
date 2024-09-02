3214c93a49e1678a6ba4fc37dc42e238f0a8c8a7
Aidan Timson <aidan@timmo.dev>
2024-09-02 20:27:28 +0100

    Homepage link

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@
 import { useMemo } from "react";
 import { usePathname } from "next/navigation";
 import { CloudSun } from "lucide-react";
+import Link from "next/link";
 
 import { ThemeToggle } from "~/components/theme-toggle";
 
@@ -11,8 +12,14 @@ export function Header() {
   const isHomePage = useMemo<boolean>(() => pathname === "/", [pathname]);
 
   return (
-    <header className="flex w-full items-center justify-between gap-2 px-4 py-3">
-      {isHomePage ? <div /> : <CloudSun />}
+    <header className="flex w-full items-center justify-between gap-2 px-3 py-2">
+      {isHomePage ? (
+        <div />
+      ) : (
+        <Link href="/">
+          <CloudSun className="h-[1.2rem] w-[1.2rem]" />
+        </Link>
+      )}
       <ThemeToggle />
     </header>
   );
