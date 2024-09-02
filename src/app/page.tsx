24a649675930d0c1d5e356ce46de4a6fa63e1b23
Aidan Timson <aidan@timmo.dev>
2024-09-02 22:02:17 +0100

    Reduce padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { LocationForm } from "~/app/_components/location-form";
 export default function HomePage() {
   return (
     <div
-      className="container flex flex-col items-center justify-center gap-12 px-4 py-16"
+      className="container flex flex-col items-center justify-center gap-12 p-4"
       role="main"
     >
       <h1 className="flex select-none flex-row items-center gap-2 text-5xl font-extrabold tracking-tight text-white delay-200 duration-300 animate-in fade-in sm:text-[5rem]">
@@ -13,8 +13,11 @@ export default function HomePage() {
         Weather
       </h1>
       <section className="flex flex-col items-center gap-3 delay-300 duration-300 animate-in fade-in">
-        <h2 className="text-2xl font-bold text-white">Location</h2>
+        <h2 className="text-2xl font-bold text-white">Set Location</h2>
         <LocationForm />
+      </section>
+      <section className="flex flex-col items-center gap-3 delay-300 duration-300 animate-in fade-in">
+        <h2 className="text-2xl font-bold text-white">Forecast</h2>
       </section>
     </div>
   );
