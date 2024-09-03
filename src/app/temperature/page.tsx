6d3263c660a1a1b6dff8beab488ed1eaa632329e
Aidan Timson <aidan@timmo.dev>
2024-09-03 23:06:33 +0100

    Add placeholder temperature page

---
+++ src/app/temperature/page.tsx
@@ -0,0 +1,9 @@
+export default function TemperaturePage() {
+  return (
+    <>
+      <section className="flex w-full flex-col items-center gap-3 delay-300 duration-300 animate-in fade-in">
+        <h2 className="text-2xl font-bold">Temperature</h2>
+      </section>
+    </>
+  );
+}
