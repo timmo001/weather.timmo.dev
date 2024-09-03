4039c736f2c9d85c51ec1ef2cfedc8f8c5df7326
Aidan Timson <aidan@timmo.dev>
2024-09-03 23:05:41 +0100

    Move top section to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,22 @@
 import { type Metadata } from "next";
 import { ThemeProvider } from "next-themes";
 import { GeistSans } from "geist/font/sans";
+import { CloudSun } from "lucide-react";
 
 import "~/styles/globals.css";
 
+import { Button } from "~/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "~/components/ui/dialog";
 import { Header } from "~/components/header";
 import { QueryProvider } from "~/components/providers/query";
+import { LocationForm } from "~/app/_components/location-form";
 
 export const metadata: Metadata = {
   title: "Weather",
@@ -41,10 +52,38 @@ export default function RootLayout({
           enableSystem
         >
           <QueryProvider>
-            <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-sky-500 to-white transition-all dark:from-sky-950 dark:to-black">
+            <div className="flex min-h-screen flex-col items-center bg-gradient-to-b from-sky-500 to-white transition-all dark:from-sky-950 dark:to-black">
               <Header />
-              {children}
-            </main>
+              <main
+                className="container flex flex-col items-center justify-center gap-12 p-4"
+                role="main"
+              >
+                <h1 className="flex select-none flex-row items-center gap-2 text-5xl font-extrabold tracking-tight delay-200 duration-300 animate-in fade-in sm:text-[5rem]">
+                  <CloudSun className="h-16 w-16" />
+                  Weather
+                </h1>
+                <section className="flex w-full flex-col items-center gap-3 delay-300 duration-300 animate-in fade-in">
+                  <Dialog>
+                    <DialogTrigger asChild>
+                      <Button className="text-2xl font-bold" size="lg">
+                        Set Location
+                      </Button>
+                    </DialogTrigger>
+                    <DialogContent className="w-full">
+                      <DialogHeader>
+                        <DialogTitle>Set Location</DialogTitle>
+                        <DialogDescription>
+                          Set your location to get the weather forecast
+                        </DialogDescription>
+                      </DialogHeader>
+                      <LocationForm />
+                    </DialogContent>
+                  </Dialog>
+                </section>
+
+                {children}
+              </main>
+            </div>
           </QueryProvider>
         </ThemeProvider>
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,10 @@
-import { CloudSun } from "lucide-react";
 import { ForecastDaily } from "~/app/_components/forecast-daily";
 import { ForecastHourly } from "~/app/_components/forecast-hourly";
-
-import { Button } from "~/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "~/components/ui/dialog";
 import { ForecastNow } from "~/app/_components/forecast-now";
-import { LocationForm } from "~/app/_components/location-form";
 
-export default function HomePage() {
+export default function TemperaturePage() {
   return (
-    <div
-      className="container flex flex-col items-center justify-center gap-12 p-4"
-      role="main"
-    >
-      <h1 className="flex select-none flex-row items-center gap-2 text-5xl font-extrabold tracking-tight delay-200 duration-300 animate-in fade-in sm:text-[5rem]">
-        <CloudSun className="h-16 w-16" />
-        Weather
-      </h1>
-      <section className="flex w-full flex-col items-center gap-3 delay-300 duration-300 animate-in fade-in">
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button className="text-2xl font-bold" size="lg">
-              Set Location
-            </Button>
-          </DialogTrigger>
-          <DialogContent className="w-full">
-            <DialogHeader>
-              <DialogTitle>Set Location</DialogTitle>
-              <DialogDescription>
-                Set your location to get the weather forecast
-              </DialogDescription>
-            </DialogHeader>
-            <LocationForm />
-          </DialogContent>
-        </Dialog>
-      </section>
+    <>
       <section className="flex w-full flex-col items-center gap-3 delay-300 duration-300 animate-in fade-in">
         <h2 className="text-2xl font-bold">Now</h2>
         <ForecastNow />
@@ -49,6 +12,6 @@ export default function HomePage() {
         <ForecastHourly />
         <ForecastDaily />
       </section>
-    </div>
+    </>
   );
 }
