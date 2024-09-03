2693155ce9817dd15b0513be60d249eee80d74c1
Aidan Timson <aidan@timmo.dev>
2024-09-03 21:32:14 +0100

    Alter icon

--- src/app/icon.tsx
+++ src/app/icon.tsx
@@ -24,7 +24,7 @@ export default function Icon() {
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          color: "white",
+          color: "#cbd5e1",
         }}
       >
         <svg
