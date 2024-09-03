4b7a836c903271e2feb2c3414ee02f0f523cfff9
Aidan Timson <aidan@timmo.dev>
2024-09-03 22:45:04 +0100

    Add navigation

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 
+import { Button } from "~/components/ui/button";
+
 export default function NotFound() {
   return (
     <main className="flex min-h-96 flex-col items-center justify-start">
       <section className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <h2 className="text-center text-2xl font-light">404: Page not found</h2>
         <Link href="/">
-          <button className="">Return Home</button>
+          <Button>Return Home</Button>
         </Link>
       </section>
     </main>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useMemo } from "react";
 import { usePathname } from "next/navigation";
-import { CloudSun } from "lucide-react";
 import Link from "next/link";
 
 import { ThemeToggle } from "~/components/theme-toggle";
@@ -12,14 +11,13 @@ export function Header() {
   const isHomePage = useMemo<boolean>(() => pathname === "/", [pathname]);
 
   return (
-    <header className="flex w-full items-center justify-between gap-2 px-3 py-2">
-      {isHomePage ? (
-        <div />
-      ) : (
-        <Link href="/">
-          <CloudSun className="h-[1.2rem] w-[1.2rem] delay-200 duration-300 animate-in fade-in" />
+    <header className="flex w-full flex-wrap items-center justify-between gap-2 px-3 py-2">
+      <nav className="flex flex-wrap items-center gap-6 delay-200 duration-300 animate-in fade-in">
+        <Link className="ms-2" href="/">
+          Home
         </Link>
-      )}
+        <Link href="/forecast">Forecast</Link>
+      </nav>
       <ThemeToggle />
     </header>
   );
