5e5ff1fa9c3a3c15d4f7796254989b683ba9b24f
Aidan Timson <aidan@timmo.dev>
2024-09-03 22:50:05 +0100

    Improve 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
   return (
     <main className="flex min-h-96 flex-col items-center justify-start">
       <section className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h2 className="text-center text-2xl font-light">404: Page not found</h2>
+        <h2 className="text-center text-3xl font-bold">404: Page not found</h2>
         <Link href="/">
           <Button>Return Home</Button>
         </Link>
