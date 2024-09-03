697a709e238f86dffde51b04f0a1cbf2494eb3df
Aidan Timson <aidan@timmo.dev>
2024-09-03 23:10:53 +0100

    Add placeholder

--- src/app/temperature/page.tsx
+++ src/app/temperature/page.tsx
@@ -9,6 +9,7 @@ export default function TemperaturePage() {
     <>
       <section className="flex w-full flex-col items-center gap-3 delay-300 duration-300 animate-in fade-in">
         <h2 className="text-2xl font-bold">Temperature</h2>
+        <p>Coming soon...</p>
       </section>
     </>
   );
