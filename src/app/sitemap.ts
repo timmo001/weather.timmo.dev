0f90522377027dd4c70efc3fa23cb6ab9f3e893e
Aidan Timson <aidan@timmo.dev>
2024-09-04 18:31:20 +0100

    Merge sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,16 +6,10 @@ export const baseUrl = "https://weather.timmo.dev";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const now = new Date();
-  const result: MetadataRoute.Sitemap = [
-    {
-      url: baseUrl,
-      lastModified: now,
-    },
-    ...navItems.map(({ href }) => ({
-      url: `${baseUrl}${href}`,
-      lastModified: now,
-    })),
-  ];
+  const result: MetadataRoute.Sitemap = navItems.map(({ href }) => ({
+    url: `${baseUrl}${href}`,
+    lastModified: now,
+  }));
 
   return result;
 }
