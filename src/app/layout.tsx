b9396acd036cd3ea05aeda3af6f3e5e5f56ea63f
Aidan Timson <aidan@timmo.dev>
2024-09-04 17:59:15 +0100

    Format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,8 +28,6 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
-
-
   return (
     <html
       className={`${GeistSans.variable}`}
